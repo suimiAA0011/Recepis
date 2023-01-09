43b300cd399f2260cd1dcc54e4c55c097e0cec03
suimiAA0011 <sm.seemo25@gmail.com>
2023-01-09 07:07:54 +0300

    Initial Commit

---
+++ Res/ResApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResApp.swift
+//  Res
+//
+//  Created by Salma on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ResApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
